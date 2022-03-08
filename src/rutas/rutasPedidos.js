d05f5448425be9c7c8d12abcfe50d729e45f556b
MariaJose0102 <79732002+MariaJose0102@users.noreply.github.com>
2022-03-08 11:29:38 -0600

    correccion

--- src/rutas/rutasPedidos.js
+++ src/rutas/rutasPedidos.js
@@ -3,7 +3,7 @@ const controladorPedidos = require('../controladores/controladorPedidos');
 const router = Router();
 
 router.get('/', controladorPedidos.Inicio);
-router.get('/listar', controladorPedidos.listarTipoUsuarios);
+router.get('/listar', controladorPedidos.listarPedidos);
 router.post('/guardar', controladorPedidos.guardar);
 router.put('/modificar', controladorPedidos.modificar);
 router.delete('/eliminar', controladorPedidos.eliminar);
